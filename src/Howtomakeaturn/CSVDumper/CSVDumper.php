db16af514e34ff2ac50b6f171dfa176f371435df
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-12-21 15:19:27 +0800

    Add arrayToCsv function.

--- src/Howtomakeaturn/CSVDumper/CSVDumper.php
+++ src/Howtomakeaturn/CSVDumper/CSVDumper.php
@@ -70,4 +70,31 @@ class CSVDumper{
         return $result;        
     }
     
+    /**
+      * Formats a line (passed as a fields  array) as CSV and returns the CSV as a string.
+      * Adapted from http://us3.php.net/manual/en/function.fputcsv.php#87120
+      */
+    static public function arrayToCsv( array &$fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false ) {
+        $delimiter_esc = preg_quote($delimiter, '/');
+        $enclosure_esc = preg_quote($enclosure, '/');
+
+        $output = array();
+        foreach ( $fields as $field ) {
+            if ($field === null && $nullToMysqlNull) {
+                $output[] = 'NULL';
+                continue;
+            }
+
+            // Enclose fields containing $delimiter, $enclosure or whitespace
+            if ( $encloseAll || preg_match( "/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field ) ) {
+                $output[] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $field) . $enclosure;
+            }
+            else {
+                $output[] = $field;
+            }
+        }
+
+        return implode( $delimiter, $output );
+    }    
+    
 }
